311f66a83c32542ed28e71b08b8b8f601e0a39ba
Amrinder Singh <amrinder995@gmail.com>
2024-03-27 21:03:12 -0400

    add XSS security

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
   $('#newtweet-form').on("submit", function(event) {
     event.preventDefault();
     let $formData = $(this).serialize();
-    let tweetLength = $('#tweet-text').val();
+    let tweetLength = $('#tweet-text').val().length;
     if (tweetLength <= 0) {
       alert('Incorrect submission. You must enter atleast 1 character.');
       return false;
@@ -27,51 +27,57 @@ $(document).ready(function() {
       },
       fail: (error) => {
         console.log(error);
+        alert("Something went wrong with your request.")
       }
     })
   }); 
-  const renderTweets = function (arrayOfTweetObjects) {
-    for(let tweetObject of arrayOfTweetObjects) {
-      let $tweet = createTweetElement(tweetObject);
-      $('#tweet-container').prepend($tweet);
-    }
-  }
-  const createTweetElement = function (tweetObject) {
-    let $tweet = $(
-      `<section class="tweet-show">
-        <header class="tweet-header">
-          <p id="tweet-name"><img id="tweet-avatar"src="${tweetObject.user.avatars}" alt="">${tweetObject.user.name}</p>
-          <p id="tweet-handle">${tweetObject.user.handle}</p>
-        </header>
-        <article class="tweet">${tweetObject.content.text}</article>
-        <footer id="tweet-footer">
-          <p class="timestamp">${timeago.format(tweetObject.created_at)}</p>
-          <p>
-            <i class="fa-solid fa-flag"></i>
-            <i class="fa-solid fa-retweet"></i>
-            <i class="fa-solid fa-heart"></i>
-          </p>
-        </footer>
-      </section>`
-    ) 
-  return $tweet;
-  }
-  const loadTweets = function() {
-    $.ajax({
-      method: 'GET',
-      url: 'http://localhost:8080/tweets',
-      success: (data) => {
-        renderTweets(data)
-      },
-      fail: (error) => {
-        console.log(error);
-      }
-    })
-  }
   loadTweets();
 });
 
-
+const renderTweets = function (arrayOfTweetObjects) {
+  for(let tweetObject of arrayOfTweetObjects) {
+    let $tweet = createTweetElement(tweetObject);
+    $('#tweet-container').prepend($tweet);
+  }
+}
+const createTweetElement = function (tweetObject) {
+  let escapeHTML = function(text) {
+    return $('<div>').text(text).html();
+  }
+  let tweetText = tweetObject.content.text;
+  let tweetHTML = `${escapeHTML(tweetText)}`;
+  let $tweet = $(
+    `<section class="tweet-show">
+      <header class="tweet-header">
+        <p id="tweet-name"><img id="tweet-avatar"src="${tweetObject.user.avatars}" alt="">${tweetObject.user.name}</p>
+        <p id="tweet-handle">${tweetObject.user.handle}</p>
+      </header>
+      <article class="tweet">${tweetHTML}</article>
+      <footer id="tweet-footer">
+        <p class="timestamp">${timeago.format(tweetObject.created_at)}</p>
+        <p>
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </p>
+      </footer>
+    </section>`
+  )
+  return $tweet;
+}
+const loadTweets = function() {
+  $.ajax({
+    method: 'GET',
+    url: 'http://localhost:8080/tweets',
+    success: (data) => {
+      $('#tweet-container').empty();
+      renderTweets(data)
+    },
+    fail: (error) => {
+      console.log(error);
+    }
+  })
+}
 
 
 
