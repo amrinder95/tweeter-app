4203e6588619d4eb9294cd5f50245a86a675879b
Amrinder Singh <amrinder995@gmail.com>
2024-03-27 23:17:03 -0400

    add form toggle

---
+++ public/scripts/dropdown.js
@@ -0,0 +1,12 @@
+/*
+ * Client-side JS logic goes here
+ * jQuery is already loaded
+ * Reminder: Use (and do all your DOM work in) jQuery's document ready function
+ */
+
+$(document).ready(function() {
+  $('#dropdown').on("click", function() {
+    $('.tweet-form').slideToggle(500);
+    $('#tweet-text').focus();
+  });
+});
